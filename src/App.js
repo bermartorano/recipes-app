06024f64015b765f3b3fc4e5d3c42e8babdd21b8
Felipe <felipe.raindo@gmail.com>
2023-01-26 16:02:12 -0300

    done: tests and component finished

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <BrowserRouter>
+      <div className="meals">
+        <span className="logo">TRYBE</span>
+        <object
+          className="rocksGlass"
+          type="image/svg+xml"
+          data={ rockGlass }
+        >
+          Glass
+        </object>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
